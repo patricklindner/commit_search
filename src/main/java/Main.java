21581a903871372c9391e4b9f99b47ddf4414400
patrick <patricklindner@outlook.com>
2024-01-10 12:54:52 +0100

    works doe finetuning

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
+import org.bitbucket.cowwoc.diffmatchpatch.DiffMatchPatch;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffFormatter;
 import org.eclipse.jgit.diff.EditList;
+import org.eclipse.jgit.diff.RawTextComparator;
+import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
@@ -14,46 +17,94 @@ import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 import org.eclipse.jgit.util.io.DisabledOutputStream;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException, GitAPIException {
         FileRepositoryBuilder builder = new FileRepositoryBuilder();
-        Repository repository = builder.setGitDir(new File("/Users/Patrick.Lindner/IdeaProjects/spring-framework/.git"))
+        Repository repository = builder.setGitDir(new File("/home/patrick/projects/spring-framework/.git"))
                 .readEnvironment() // scan environment GIT_* variables
                 .findGitDir() // scan up the file system tree
                 .build();
+
+        FileWriter writer = new FileWriter("filename.txt");
 
         ObjectReader objectReader = repository.newObjectReader();
 
         Git git = new Git(repository);
         Iterable<RevCommit> allCommits = git.log().all().call();
 
-        RevCommit commit = allCommits.iterator().next();
 
-        DiffFormatter diffFormatter = new DiffFormatter(DisabledOutputStream.INSTANCE);
-        diffFormatter.setRepository(repository);
-        CanonicalTreeParser oldTreeParser = new CanonicalTreeParser(null, repository.newObjectReader(), commit.getParent(0).getTree());
-        CanonicalTreeParser newTreeParser = new CanonicalTreeParser(null, repository.newObjectReader(), commit.getTree());
-        List<DiffEntry> diffs = diffFormatter.scan(oldTreeParser, newTreeParser);
+        for (RevCommit commit : allCommits) {
 
-        DiffEntry diffEntry = diffs.get(0);
-        String newPath = diffEntry.getNewPath();
-        newTreeParser.findFile(newPath);
+            if (commit.getParentCount() > 0) {
 
-        ObjectLoader loader = objectReader.open(newTreeParser.getEntryObjectId());
-        System.out.println(diffEntry.getNewPath());
-        loader.copyTo(System.out);
+                RevCommit parentCommit = commit.getParent(0);
 
+                try (ObjectReader reader = git.getRepository().newObjectReader()) {
+                    CanonicalTreeParser oldTreeIter = new CanonicalTreeParser();
+                    oldTreeIter.reset(reader, parentCommit.getTree());
+                    CanonicalTreeParser newTreeIter = new CanonicalTreeParser();
+                    newTreeIter.reset(reader, commit.getTree());
+
+                    try (DiffFormatter formatter = new DiffFormatter(DisabledOutputStream.INSTANCE)) {
+                        formatter.setRepository(git.getRepository());
+
+                        List<DiffEntry> diffs = formatter.scan(oldTreeIter, newTreeIter);
+
+                        for (DiffEntry diff : diffs) {
+
+                            String oldLine = "";
+                            String newLine = "";
+                            if (diff.getChangeType() != DiffEntry.ChangeType.ADD) {
+                                var oldLoader = objectReader.open(diff.getOldId().toObjectId());
+                                oldLine = new String(oldLoader.getBytes());
+                            }
+                            if (diff.getChangeType() != DiffEntry.ChangeType.DELETE) {
+                                var newLoader = objectReader.open(diff.getNewId().toObjectId());
+                                newLine = new String(newLoader.getBytes());
+                            }
+
+                            DiffMatchPatch dmp = new DiffMatchPatch();
+                            LinkedList<DiffMatchPatch.Diff> diffLines = dmp.diffMain(oldLine, newLine);
+
+                            for (DiffMatchPatch.Diff diffLine : diffLines) {
+                                if(diffLine.operation != DiffMatchPatch.Operation.EQUAL) {
+                                    writer.write(diffLine.operation+","+commit.getCommitTime()+","+diffLine.text+"\n");
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+            }
+        }
+
+//                DiffFormatter diffFormatter = new DiffFormatter(DisabledOutputStream.INSTANCE);
+//                diffFormatter.setRepository(repository);
+//                CanonicalTreeParser oldTreeParser = new CanonicalTreeParser(null, objectReader, parentCommit.getTree());
+//                CanonicalTreeParser newTreeParser = new CanonicalTreeParser(null, objectReader, commit.getTree());
+//                List<DiffEntry> diffs = diffFormatter.scan(oldTreeParser, newTreeParser);
 //
-//        EditList edits = diffFormatter.toFileHeader(diffs.get(0)).toEditList();
+//                for (DiffEntry diff : diffs) {
+//                    String oldPath = diff.getOldPath();
+//                    String newPath = diff.getNewPath();
+//                    newTreeParser.findFile(newPath);
+//                    oldTreeParser.findFile(oldPath);
 //
-//        edits.get(0).
+//                    ObjectLoader newLoader = objectReader.open(newTreeParser.getEntryObjectId());
+//                    ObjectLoader oldLoader = objectReader.open(oldTreeParser.getEntryObjectId());
 //
-//        ObjectLoader open = objectReader.open(commit);
-//
-//
-//        System.out.println();
+//                    DiffMatchPatch dmp = new DiffMatchPatch();
+//                    LinkedList<DiffMatchPatch.Diff> diffs1 = dmp.diffMain(new String(oldLoader.getBytes()), new String(newLoader.getBytes()));
+//                }
+
     }
+
 }
+
